86311c37b66fcb71d0fd6e1d381b3a41c691cbbb
NNTin <nguyen.ngoctindaniel@gmail.com>
2018-12-30 19:58:11 +0100

    various things

--- tamagotchi/pet.py
+++ tamagotchi/pet.py
@@ -2,6 +2,7 @@ import random
 from string import Formatter
 from datetime import datetime
 
+
 class ExtendedFormatter(Formatter):
     """An extended format string formatter
 
@@ -20,10 +21,10 @@ class ExtendedFormatter(Formatter):
 
 class Pet:
     """The actual tamagotchi pet instance."""
-    def __init__(self, info, seed: int):
+    def __init__(self, info):
         # for now determines the gender of your pet
-        self.seed = seed + len(info["dead_tamagotchis"])
-        random.seed(seed)
+        self.seed = info["owner_id"] + len(info["dead_tamagotchis"])
+        random.seed(self.seed)
 
         self.name = info["tama_name"]
         self.points = info["tama_points"]
@@ -34,6 +35,8 @@ class Pet:
         self.hunger = info["tama_hunger"]
         self.next_random_event = info["tama_next_random_event"]
         self.next_event = info["tama_next_event"]
+        self.next_poop = info["tama_next_poop"]
+        self.clean_poop = info["tama_clean_poop"]
         self.owner_id = str(info["owner_id"])
 
         gen = random.randint(0, 2)
@@ -55,9 +58,27 @@ class Pet:
 
     def get_next_event(self):
         """
-        Calculate the next event based on health, hunger and happiness.
+        Calculate the next event based on health and hunger.
+        Hunger events are fired at: 500, 300, 100, 0
+        Health events are fired at: 800, 500, 300, 0
+
+        1000 hunger lasts 48 hours. 1 hunger = 172.8 seconds
+        1000 health lasts 24 hours. 1 health =  86.4 seconds
+
+        Health only ticks down when there is poop.
         """
-        return 1561
+        hunger_event = self.hunger - (500 if self.hunger > 500 else
+                                      (300 if self.hunger > 300 else
+                                       (100 if self.hunger > 100 else 0)))
+        health_event = self.health - (800 if self.health > 800 else
+                                      (500 if self.health > 500 else
+                                       (300 if self.health > 300 else 0)))
+        hunger_event *= 172.8      # todo: commented for testing reasons
+        if self.clean_poop:
+            health_event *= 86.4
+        else:
+            health_event = 172800
+        return self.timestamp + min(hunger_event, health_event)
 
     if True:
         """
@@ -83,6 +104,29 @@ class Pet:
                 "Write `[p]tama` to see what else you can do. You decide to name {o} **{name!u}**."
             return self.cformat(t)
 
+        def hunger_health_event(self):
+            t = "It's been some time since you looked after {name!c}."
+            return self.cformat(t)
+
+        def hunger_event(self):
+            # 500 300 100 0
+            t = None
+            if self.health > 400:
+                t = "Your tamagotchi {name!c} seems to be hungry. Maybe you should feed {o}."
+            elif self.health > 200:
+                t = "You should feed {name!c}. {s}'s been starving for quite some time."
+            elif self.health > 50:
+                t = "{name!c} is about to starve to death."
+            else:
+                t = "{name!c} starved to death."
+            return self.cformat(t)
+
+        def health_event(self):
+            # todo: write a bunch of different texts
+            # 800 500 300 0
+            t = "Your pet {name!c} is in top health (cleanliness)."
+            return self.cformat(t)
+
 
 if __name__ == '__main__':
     info = {
@@ -96,9 +140,10 @@ if __name__ == '__main__':
         "tama_hunger": 951,
         "tama_next_random_event": 1546206861.234719,
         "tama_next_event": 1546213286.234719,
+        "tama_next_poop": 1546213286.234719,
+        "tama_clean_poop": False,
         "owner_id": 4564654
     }
-    p = Pet(info=info, seed=7)
-    print(p.hatch_success_2())
+    p = Pet(info=info)
 
-    print(datetime.utcnow().timestamp())
+    print(p.hunger_health_event())
--- tamagotchi/tamagotchi.py
+++ tamagotchi/tamagotchi.py
@@ -5,13 +5,24 @@ from redbot.core.commands import Context
 from .pet import Pet
 from datetime import datetime
 import random
+import asyncio
 
 
 class Tamagotchi(commands.Cog):
     """
     Take good care of your Tamagotchi
+
+    Your Tamagotchi's happiness increases when it's healthy and not hungry. The happier your Tamagotchi is the
+    more points you will earn.
+    You can have a maximum of 1000 Health and Hunger. When your Hunger hits 0 your Tamagotchi dies. Don't worry
+    you have to neglect your pet for 2 entire days for that to happen.
+    Occasionally your pet will poop. If you don't clean its litter box its health will decrease. Although your
+    pet can't die if its health drops to a certain threshold you can't earn any points.
     """
 
+    default_guild = {
+        "text_channel_id": None
+    }
     default_member = {
         "dead_tamagotchis": [],             # collection of pets that died under you
         "tama_name": None,                  # your pet name
@@ -21,9 +32,11 @@ class Tamagotchi(commands.Cog):
         "tama_happiness": None,             # 0-1000 happiness status as of timestamp
         "tama_health": None,                # 0-1000 health status as of timestamp
         "tama_hunger": None,                # 0-1000 hunger status as of timestamp
-        "tama_next_random_event": None,     # a programmed forced event that will happen
+        "tama_next_random_event": None,     # a programmed forced event: good thing you can earn points
         "tama_next_event": None,            # a programmed event that will happen, can be postponed by interacting
                                             # usually a bad thing since it means you have been neglecting it
+        "tama_next_poop": None,             # a programmed event: Your pet poops
+        "tama_clean_poop": False,           # whether you have to clean poop
         "owner_id": None                    # that's you!
     }
     conf_id = 800858686
@@ -33,13 +46,63 @@ class Tamagotchi(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, self.conf_id)
         self.config.register_member(**self.default_member)
+        self.running = True
 
-    async def _update(self, info):
+        self.bot.loop.create_task(self._check())
+
+    async def _check_future_events(self):
+        members_dict = await self.config.all_members()
+        for guild_id in members_dict.keys():
+            for member_id in members_dict[guild_id].keys():
+                _dict = members_dict[guild_id][member_id]
+                print(_dict)
+                if "tama_next_event" in _dict \
+                        and _dict["tama_next_event"] is not None \
+                        and _dict["tama_next_event"] - datetime.utcnow().timestamp() < 0:
+                    # next_event is caused when hunger or health goes below a threshold
+                    guild = self.bot.get_guild(guild_id)
+                    member = guild.get_member(member_id)
+                    await self._status_event(member)
+
+    async def _status_event(self, member: Member):
+        async with self.config.member(member)() as member_group:
+            pet = Pet(info=member_group)
+            member_group["tama_next_event"] = pet.get_next_event()
+
+            embed = Embed(color=member.color, title="Did you forget someone?",
+                          description=pet.hunger_health_event())
+            embed.add_field(name="Hunger", value=pet.hunger_event())
+            embed.add_field(name="Health", value=pet.health_event())
+            embed.set_footer(text='NNTin cogs', icon_url='https://i.imgur.com/6LfN4cd.png')
+
+            async with self.config.guild(member.guild)() as guild_group:
+                if "text_channel_id" in guild_group and guild_group["text_channel_id"]:
+                    channel = self.bot.get_channel(guild_group["text_channel_id"])
+                    await channel.send(embed=embed)
+                else:
+                    await member.send(embed=embed)
+
+            await self._update(member, member_group)
+
+    async def _check(self):
+        while self.running:
+            print(datetime.utcnow())
+            #try:
+            await self._check_future_events()
+            #except:
+                #print("Something went wrong.")
+            await asyncio.sleep(1)
+
+    def __unload(self):
+        self.running = False
+
+    async def _update(self, member: Member, member_group):
         """
         calculating the new status of the pet, method is called when
-        interacting with your pet and after a event
+        doing anything health/hunger related with your pet and after a event
         """
-        pass
+        pet = Pet(info=member_group)
+        member_group["tama_next_event"] = pet.get_next_event()
 
     @commands.group()
     async def tama(self, ctx):
@@ -67,22 +130,23 @@ class Tamagotchi(commands.Cog):
                 member_group["tama_health"] = random.randint(900, 1000)
                 member_group["tama_hunger"] = random.randint(900, 1000)
                 member_group["tama_next_random_event"] = unix_time + random.randint(28800, 36000)
+                member_group["tama_next_poop"] = unix_time + random.randint(28800, 36000)
                 member_group["owner_id"] = ctx.author.id
 
-                pet = Pet(info=member_group, seed=ctx.author.id)
+                pet = Pet(info=member_group)
                 member_group["tama_next_event"] = pet.get_next_event()
 
                 embed = Embed(color=ctx.author.color, title="It's your lucky day!".format(name),
                               description=pet.hatch_success_1())
                 embed.add_field(name="What now?", value=pet.hatch_success_2())
             else:
-                await self._update(member_group)
+                await self._update(ctx.author, member_group)
 
-                pet = Pet(info=member_group, seed=ctx.author.id)
+                pet = Pet(info=member_group)
                 embed = Embed(color=ctx.author.color, title="Try another day.",
                               description=pet.hatch_fail())
-                member_group["tama_points"] = int(0.9 * member_group["tama_points"])
-                member_group["tama_happiness"] = int(0.8 * member_group["tama_happiness"])
+                member_group["tama_points"] = 0.9 * member_group["tama_points"]
+                member_group["tama_happiness"] = 0.8 * member_group["tama_happiness"]
             embed.set_footer(text='NNTin cogs', icon_url='https://i.imgur.com/6LfN4cd.png')
             await ctx.send(embed=embed)
 
@@ -95,6 +159,7 @@ class Tamagotchi(commands.Cog):
         async with self.config.member(ctx.author)() as member_group:
             member_group.clear()
             await ctx.tick()
+
 
     @tama.command()
     async def test(self, ctx: Context):
